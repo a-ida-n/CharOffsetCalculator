7ef70c02da6be90b1c69a01475939b12d9a73b54
a-ida-n <92699100+a-ida-n@users.noreply.github.com>
2021-12-05 19:38:25 -0500

    Initial code commit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+// TODO: COmments
+
+#include <iostream>
+
+char character(char, int);
+
+int main() 
+{
+
+	try
+	{
+		std::cout << character('a', 1) << '\n';
+	}
+	catch (std::string errorMessage)
+	{
+		std::cout << errorMessage << '\n';
+	}
+
+	try
+	{
+		std::cout << character('a', -1) << '\n';
+	}
+	catch (std::string errorMessage)
+	{
+		std::cout << errorMessage << '\n';
+	}
+
+	try
+	{
+		std::cout << character('Z', -1) << '\n';
+	}
+	catch (std::string errorMessage)
+	{
+		std::cout << errorMessage << '\n';
+	}
+
+	try
+	{
+		std::cout << character('?', 5) << '\n';
+	}
+	catch (std::string errorMessage)
+	{
+		std::cout << errorMessage << '\n';
+	}
+
+	try
+	{
+		std::cout << character('A', 32) << '\n';
+	}
+	catch (std::string errorMessage)
+	{
+		std::cout << errorMessage << '\n';
+	}
+
+
+	std::cout << '\n';
+	system("pause");
+	return 0;
+}
+
+char character(char start, int offset)
+{
+	std::string badRange = "invalidRangeException";
+	std::string badChar = "invalidCharacterException";
+	std::string badCase = "invalidCaseException";
+
+	if (isalpha(start) == false)
+	{
+		// The char given is not an alphabetic character
+		throw(badChar);
+	}
+
+	char newchar = static_cast<char>(start + offset);
+
+	if (isalpha(newchar) == false)
+	{
+		// The new char given is no longer an alphabetic character
+		throw(badRange);
+	}
+
+	if ((isupper(start) == false && isupper(newchar) == true) || (isupper(start) == true && isupper(newchar) == false))
+	{
+		// Input and output are not the same case (e.g. input A output b)
+		throw(badCase);
+	}
+
+
+	return newchar;
+}
